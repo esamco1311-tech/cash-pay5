0b9c4e03b26cadea36fe2106889c49385e142b5a
esamco1311-tech <esamco1311@gmail.com>
2026-08-17 23:15:36 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'cashpay-v12';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './assets/logo.png',
+  './assets/operators/vodafone.webp',
+  './assets/operators/etisalat.png',
+  './assets/operators/orange.webp',
+  './assets/operators/instapay.webp'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((res) => {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+          return res;
+        })
+        .catch(() => cached);
+    })
+  );
+});
